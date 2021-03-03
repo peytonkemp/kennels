3b7ba173d7416bfbee798fb2f0414ac0166b81b2
peytonkemp <peyton.kemp@gmail.com>
2021-03-03 15:04:48 -0600

    releasing animals with DELETE method

--- src/components/animal/AnimalProvider.js
+++ src/components/animal/AnimalProvider.js
@@ -30,6 +30,13 @@ export const AnimalProvider = (props) => {
             .then(res => res.json())
     }
 
+    const releaseAnimal = animalId => {
+        return fetch(`http://localhost:8088/animals/${animalId}`, {
+            method: "DELETE"
+        })
+            .then(getAnimals)
+    }
+
     /*
         You return a context provider which has the
         `animals` state, `getAnimals` function,
@@ -38,7 +45,7 @@ export const AnimalProvider = (props) => {
     */
     return (
         <AnimalContext.Provider value={{
-            animals, getAnimals, addAnimal, getAnimalById
+            animals, getAnimals, addAnimal, getAnimalById, releaseAnimal
         }}>
             {props.children}
         </AnimalContext.Provider>
