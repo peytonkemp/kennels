db07290740fa39508671b23aa99916b73da6a600
peytonkemp <peyton.kemp@gmail.com>
2021-03-04 10:45:16 -0600

    added animal search functionality. Refactored AnimalList. Updated ApplicationViews.js to show <AnimalSearch />

--- src/components/animal/AnimalList.js
+++ src/components/animal/AnimalList.js
@@ -1,4 +1,4 @@
-import React, { useContext, useEffect } from "react"
+import React, { useContext, useEffect, useState } from "react"
 import { useHistory } from "react-router-dom" // import from libraries before your local modules
 import { AnimalContext } from "./AnimalProvider"
 import { AnimalCard } from "./AnimalCard"
@@ -9,35 +9,47 @@ import { LocationContext } from "../locations/LocationProvider"
 
 export const AnimalList = () => {
   // This state changes when `getAnimals()` is invoked below
-  const { animals, getAnimals } = useContext(AnimalContext)
+  const { animals, getAnimals, searchTerms } = useContext(AnimalContext)
   const { customers, getCustomers } = useContext(CustomerContext)
   const { locations, getLocations } = useContext(LocationContext)
-  // The useHistory hook let's us tell React which route we want to visit. We will use it to tell React to render the animal form component.
+ 
+  // Since you are no longer ALWAYS displaying all of the animals
+  const [ filteredAnimals, setFiltered ] = useState([])
   const history = useHistory()
+  
   //useEffect - reach out to the world for something
   useEffect(() => {
-    console.log("Fetching animals data from API")
-    getLocations()
-      .then(getCustomers)
-      .then(getAnimals)
+    getAnimals()
+}, [])
 
-  }, [])
 
+    // useEffect dependency array with dependencies - will run if dependency changes (state)
+  // searchTerms will cause a change
+  useEffect(() => {
+    if (searchTerms !== "") {
+      // If the search field is not blank, display matching animals
+      const subset = animals.filter(animal => animal.name.toLowerCase().includes(searchTerms))
+      setFiltered(subset)
+    } else {
+      // If the search field is blank, display all animals
+      setFiltered(animals)
+    }
+  }, [searchTerms, animals])
 
   return (
-    <div className="animals">
-      <h2>Animals</h2>
-		      <button onClick={() => {history.push("/animals/create")}}>
-            Add Animal
-          </button>
-          {
-            animals.map(animalObject => {
-            const owner = customers.find(c => c.id === animalObject.customerId)
-            const location = locations.find(l => l.id === animalObject.locationId)
-            return <AnimalCard key={animalObject.id} animal={animalObject} owner={owner} location={location} />
-            })
-          }
-        </div>
+    <>
+      <h1>Animals</h1>
 
+      <button onClick={() => history.push("/animals/create")}>
+          Make Reservation
+      </button>
+      <div className="animals">
+      {
+        filteredAnimals.map(animal => {
+          return <AnimalCard key={animal.id} animal={animal} />
+        })
+      }
+      </div>
+    </>
   )
 }
